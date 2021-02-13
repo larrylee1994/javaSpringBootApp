4100c75cf6433b0fde190f8ff24b5454510a0e9c
Larry <larrylee1994@gmail.com>
2021-02-12 20:28:11 -0500

    added Authors Controller

--- src/main/java/com/larrylee1994/webapp/controllers/AuthorController.java
+++ src/main/java/com/larrylee1994/webapp/controllers/AuthorController.java
@@ -1,2 +1,22 @@
-package com.larrylee1994.webapp.controllers;public class AuthorController {
+package com.larrylee1994.webapp.controllers;
+
+import com.larrylee1994.webapp.domain.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
 }
